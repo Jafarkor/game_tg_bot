9fe44aab3572c4a540526c989764d6127541074e
Jafar Korchagin <korjafar@gmail.com>
2024-07-28 22:55:59 +0300

    Логгирование и рефакторинг

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
 
 
+logger = logging.getLogger(__name__)
+
 async def main():
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    logger.info('Starting bot')
 
     config: Config = load_config()
 
-    bot = Bot(token=config.tg_bot.token)
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher()
 
     dp.include_router(user_handlers.router)
--- servises/servises.py
+++ servises/servises.py
@@ -1,8 +1,7 @@
 import random
-things: dict[str, str] = {'1': '🪨', '2': '✂️', '3': '📄'}
 
 def bot_choise(player: str) -> str:
-    bot = things[str(random.randint(1, 3))]
+    bot = random.choice(['🪨', '✂️', '📄'])
     if bot == player:
         return (bot, 'draw')
     elif (bot == '🪨' and player == '✂️') or (bot == '✂️' and player == '📄') or (bot == '📄' and player == '🪨'):
